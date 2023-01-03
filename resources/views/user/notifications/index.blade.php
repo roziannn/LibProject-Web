7c44861796be4181dcbf550d3aa53256fec7021a
Firda Rosiana T <firdaroziana10@gmail.com>
2023-01-03 23:08:33 +0700

    display for notifications

--- resources/views/user/notifications/index.blade.php
+++ resources/views/user/notifications/index.blade.php
@@ -4,16 +4,44 @@
 @section('container')
     <div class="container">
         <div class="row my-3">
-            <div class="card-body text-center">
-                <div class="card-header">
-                    <h3>Notifikasi</h3>
+            <div class="col-lg-12">
+                <div class="card-body">
+                    <div class="card-header">
+                        <h3>Notifikasi</h3>
+                        <hr>
+                    </div>
+                    @foreach ($notifs as $notif)
+                        <a class="link-post" href="/posts/{{ $notif->post->slug }}">
+                            <div class="box-message">
+                                {{ $notif->message }}
+                                <br><span class="message-time">{{ $notif->created_at->diffForhumans() }}</span>
+                            </div>
+                        </a>
+                    @endforeach
                 </div>
-                @foreach ($notifs as $notif)
-                    <li>
-                        {{ $notif->message }}
-                    </li>
-                @endforeach
             </div>
         </div>
     </div>
 @endsection
+
+<style>
+    .link-post {
+        text-decoration: none;
+        color: #333;
+    }
+
+
+    .box-message {
+        margin: 10px 0px 10px 0px;
+        background: #f8f8f8;
+        padding: 15px;
+        border-radius: 8px;
+        color: #333;
+        font-weight: 600;
+    }
+
+    .message-time {
+        font-size: 13px;
+        font-weight: 400;
+    }
+</style>
