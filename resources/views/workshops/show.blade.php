6bcf4533eb133b37553a4ad44ea3be90b91aaf43
Firda Rosiana T <firdaroziana10@gmail.com>
2023-07-23 00:17:31 +0700

    update layout workshop/show/content-detailed

--- resources/views/workshops/show.blade.php
+++ resources/views/workshops/show.blade.php
@@ -1,23 +1,58 @@
 @extends('layouts.main')
 @include('partials.navbar')
 @section('container')
-
-<div class="row mb-5">
-    <div class="col-lg-12">
-        <div class="content-info d-flex align-items-center justify-content-between mb-3 p-5">
-            <div class="col-lg-6">
-                <h4>{{ $workshop->workshop_name }}</h4>
-                <p>
-                    {!! $workshop->desc !!}
-                </p>
+    <div class="row mb-5">
+        <div class="col-lg-12">
+            <div class="btn-back pb-3 col-lg-9">
+                <a href="/workshop"class="text-decoration-none"> <i class="bi bi-arrow-left"></i></a>
             </div>
-            <div class="col-lg-3">
-                <img src="{{ asset('storage/' . $workshop->workshop_img) }}" alt=""
-                class="img-fluid">
+            <div class="content-workshop-info d-flex align-items-start justify-content-between mb-3 ">
+                <div class="col-lg-9">
+                    <div class="content-workshop-detail">
+                        <h4>{{ $workshop->workshop_name }}</h4>
+                        <p>
+                            {!! $workshop->desc !!}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="content-workshop-img">
+                        <img src="{{ asset('storage/' . $workshop->workshop_img) }}" alt="" class="img-fluid"
+                            width="300" height="250">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-@include('partials.footer')
+    @include('partials.footer')
 @endsection
+
+<style>
+    .col-lg-9 {
+        padding: 0 100px;
+    }
+
+    .content-workshop-detail {
+        flex: 1;
+    }
+
+
+
+    @media (max-width: 998px) {
+        .col-lg-9 {
+            padding: 0 !important;
+        }
+
+        .content-workshop-info {
+            flex-direction:  column-reverse;
+            
+        }
+
+        .img-fluid {
+            margin: 15px;
+        }
+
+        
+    }
+</style>
