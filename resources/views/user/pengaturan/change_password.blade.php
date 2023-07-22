06a2bdf21cb080e33f8b0c62fee537f9d4b5ac4c
Firda Rosiana T <firdaroziana10@gmail.com>
2023-07-22 22:04:11 +0700

    fix layout for input changes password with dom for eventlistener

--- resources/views/user/pengaturan/change_password.blade.php
+++ resources/views/user/pengaturan/change_password.blade.php
@@ -2,39 +2,77 @@
 @include('partials.navbar')
 
 @section('container')
+    <div class="row mb-5">
+        @include('dashboard.layouts.account.settings.sidebar')
+        <div class="col-lg-9">
+            <div class="card profile-content p-4">
+                <strong class="fs-5 border-bottom">Ubah Kata Sandi</strong>
+                <form action="{{ route('account.password.update') }}">
+                    @method('patch')
+                    @csrf
 
-@include('dashboard.layouts.account.settings.sidebar')
 
-<div class="col-md-9 pl-md-0">
-    <form action="{{ route('account.update') }}">
-        <div class="card user-settings__wrapper">
-            <h4 class="user-settings__title">Ubah Password</h4>
-            <div class="form-group">
-                <div class="alert alert-warning font-weight-medium small">
-                    Isi jika Anda ingin mengubah password.
-                </div>
+                    <div class="form-group form row">
+                        <div class="col-12">
+                            <label for="new-password">Kata Sandi Baru<span class="text-danger">*</span></label>
+                            <div class="input-group">
+                                <input type="password" name="new-password" class="form-control" id="new-password" required
+                                    placeholder="Enter Password">
+                                <span class="input-group-text new-password">
+                                    <i class="fas fa-eye"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group form row">
+                        <div class="col-12">
+                            <label for="confirm-new-password">Kata Sandi Baru<span class="text-danger">*</span></label>
+                            <div class="input-group">
+                                <input type="password" name="confirm-new-password" class="form-control"
+                                    id="confirm-new-password" required placeholder="Enter Password">
+                                <span class="input-group-text confirm-new-password">
+                                    <i class="fas fa-eye"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="mt-5 text-right">
+                            <input type="submit" class="btn btn-primary px-3" value="Simpan Perubahan">
+                        </div>
+                    </div>
+                </form>
             </div>
-
-            <div class="form-group form row">
-                <div class="col-md-9">
-                    <label class="control-label font-weight-bold" for="new-password">Password Baru 
-                        <span class="text-danger">*</span>
-                    </label>
-                    <input id="new-password" class="col-md-6 form-control font-weight-medium" placeholder="Masukkan password baru" autocomplete="off" required="" minlength="8" data-parsley-required-message="Password tidak boleh kosong" data-parsley-minlength-message="Password minimal terdiri dari 8 karakter" data-target="passwordUpdate.newPassword" name="new_password" type="password" value="">
-                </div>
-            </div>
-            <div class="form-group form row">
-                <div class="col-md-9">
-                    <label class="control-label font-weight-bold" for="new-password">Konfirmasi Password Baru
-                        <span class="text-danger">*</span>
-                    </label>
-                    <input id="new-password" class="col-md-6 form-control font-weight-medium" placeholder="Konfrimasi Password" autocomplete="off" required="" minlength="8" data-parsley-required-message="Password tidak boleh kosong" data-parsley-minlength-message="Password minimal terdiri dari 8 karakter" data-target="passwordUpdate.newPassword" name="new_password" type="password" value="">
-                </div>
-            </div>
-            <button type="submit" class="savebtn btn btn-primary px-3 font-weight-bold" data-label="Kirim Permintaan" style="margin-top:12px ;">Simpan Password</button>
         </div>
-    </form>
-</div>   
+    </div>
 @endsection
 
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        function togglePasswordVisibility(passwordInput, eyeIcon) {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash');
+                eyeIcon.setAttribute('title', 'Sembunyikan');
+                eyeIcon.parentNode.setAttribute('aria-label', 'Sembunyikan');
+            } else {
+                passwordInput.type = 'password';
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye');
+                eyeIcon.setAttribute('title', 'Lihat');
+                eyeIcon.parentNode.setAttribute('aria-label', 'Lihat');
+            }
+        }
 
+        document.querySelector('.new-password').addEventListener('click', function(event) {
+            event.preventDefault();
+            var passwordInput = document.getElementById('new-password');
+            togglePasswordVisibility(passwordInput, this.querySelector('i'));
+        });
+
+        document.querySelector('.confirm-new-password').addEventListener('click', function(event) {
+            event.preventDefault();
+            var passwordInput = document.getElementById('confirm-new-password');
+            togglePasswordVisibility(passwordInput, this.querySelector('i'));
+        });
+    });
+</script>
